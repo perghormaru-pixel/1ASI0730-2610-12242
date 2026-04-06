0ef607a714ddac64774451f6d5fc0de7746e2eb2
alumnos <perghormaru@gmail.com>
2026-04-06 11:05:43 -0500

    feat(address): add address value object

---
+++ ACME.OOP/Shared/Domain/Model/ValueObjects/Address.cs
@@ -0,0 +1,49 @@
+namespace oop_sample.Shared.Domain.Model.ValueObjects;
+
+/// <summary>
+///  Represents an international physical address value object 
+/// </summary>
+
+public record Address
+{
+ public string Street;
+ public string Number;
+ public string City;
+ public string? StateOrRegion;
+ public string PostalCode;
+ public string Country;
+ 
+ /// <summary>
+ /// Creates a new instance of <see cref="Address"/>
+ /// </summary>
+ /// <param name="street">the address street, which must not be null or blank</param>
+ /// <param name="number">the address number, which must not be null or blank</param>
+ /// <param name="city">the address city, which must not be null or blank</param>
+ /// <param name="stateOrRegion">the state or Region</param>
+ /// <param name="postalCode">the address postal code,which must not be null or blank</param>
+ /// <param name="country">the address country, which must not be null or blank</param>
+ /// <exception cref="ArgumentException">thrown when any required parameter is null or blank</exception>
+ 
+ public Address(string street, string number, string city, string? stateOrRegion, string postalCode, string country) 
+ {
+  if(string.IsNullOrEmpty(street)) throw new ArgumentException("Street cannot be null or empty");
+  if(string.IsNullOrEmpty(number)) throw new ArgumentException("Number cannot be null or empty");
+  if(string.IsNullOrEmpty(city)) throw new ArgumentException("City cannot be null or empty");
+  if(string.IsNullOrEmpty(postalCode)) throw new ArgumentException("State or city cannot be null or empty");
+  if(string.IsNullOrEmpty(country)) throw new ArgumentException("Country cannot be null or empty");
+  Street = street;
+  Number = number;
+  City = city;
+  StateOrRegion = stateOrRegion;
+  PostalCode = postalCode;
+  Country = country;
+ }
+ 
+ /// <summary>
+ /// Returns the string representation of the address in the format: "Street, Number, City, Region, PostalCode, Country"
+ /// </summary>
+ /// <returns></returns>
+ 
+ public override string ToString() => $"{Street}, {Number}, {City}, {StateOrRegion}, {PostalCode}, {Country}";
+ 
+}
